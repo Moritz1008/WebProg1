c8ee5a7d3c0a16e7bd7e6376d1d33fd1ef9624b5
Moritz Riedel <moritz.riedel@outlook.com>
2023-04-08 20:13:05 +0200

    Da war plötzlich was drinnen

---
+++ src/components/LiveData.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <span style="color: black" id="Rec">Recognised</span>
+  </div>
+  <div>
+    <p>{{ EAN }}</p>
+  </div>
+</template>
+
+<script>
+import {useEANstore} from "../stores/EANs";
+
+export default {
+  name: "LiveData",
+
+  data () {
+    return {
+      EAN: 0
+    }
+  },
+  setup() {
+    const path = useEANstore()
+
+    let StoreElement = path.compareEAN()
+    let StoreEAN = path.getStoreEAN
+    let StoreShow = path.getStoreShow
+
+    return { path, StoreElement, StoreEAN, StoreShow }
+  },
+  // Ändert Farbe, wenn die EAN bekannt ist 
+  mounted: {
+    changeColor() {
+      if (this.StoreElement!=0){
+        console.log("is net so gagge")
+        document.getElementById('Rec').style.color = "green";
+      } else {
+        console.log("is gagge")
+        document.getElementById('Rec').style.color = "red";
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
